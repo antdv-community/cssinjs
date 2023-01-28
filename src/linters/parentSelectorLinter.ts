2154ca8fd0a058d1047fd52f8129d14ffdb049b3
aibayanyu20 <aibayanyu@qq.com>
2023-01-28 17:05:50 +0800

    feat: add node linter

---
+++ src/linters/parentSelectorLinter.ts
@@ -0,0 +1,14 @@
+import type { Linter } from '..'
+import { lintWarning } from './utils'
+
+const linter: Linter = (key, value, info) => {
+  if (
+    info.parentSelectors.some((selector) => {
+      const selectors = selector.split(',')
+      return selectors.some(item => item.split('&').length > 2)
+    })
+  )
+    lintWarning('Should not use more than one `&` in a selector.', info)
+}
+
+export default linter
